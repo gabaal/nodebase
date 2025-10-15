1ac8806500f65cf83ea6adb25b247a8dfd437cf2
Gerald Baalham <geraldbaalham@live.co.uk>
2025-10-15 10:42:28 +0100

    feat: implement TRPC with React Query for improved data fetching and state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
-
+import {caller} from '@/trpc/server';
 
 export default async function Home() {
-
+const users = await caller.getUsers();
+console.log(users);
   return (
     <div>
       <h1 className="text-5xl">Gerald</h1>
