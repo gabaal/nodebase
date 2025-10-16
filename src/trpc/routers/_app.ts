9296bd026ad54be22816558e4c89cc73580ff936
Gerald Baalham <geraldbaalham@live.co.uk>
2025-10-16 12:36:58 +0100

    feat: add Inngest integration and workflow model
    - Added Inngest client and functions for event handling.
    - Created a new workflow model in Prisma schema.
    - Updated app router to include getWorkflows and createWorkflow procedures.
    - Implemented API route for Inngest functions.
    - Modified page component to utilize React Query for workflows and create new workflows.
    - Added migration for the new workflow table in the database.

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,27 @@
 
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+
+import { inngest } from '@/inngest/client';
+import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure
+  getWorkflows: protectedProcedure
     .query(({ctx}) => {
       console.log({userId: ctx.auth.user.id})
-      return prisma.user.findMany({
-        where: {
-          id: ctx.auth.user.id
-        }
-      }); 
+      return prisma.workflow.findMany()
     }),
+    createWorkflow: protectedProcedure.mutation(async ()=> {
+      await inngest.send({
+        name: 'test/hello.world',
+        data: {
+          email: "gerald"
+        }
+      })
+      return prisma.workflow.create({
+        data: {
+          name: 'New Workflow 2'
+        }
+      })
+    })
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
