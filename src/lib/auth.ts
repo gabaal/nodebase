9aa87fdb6e5b3064cefaeefce43f45a670148242
Gerald Baalham <geraldbaalham@live.co.uk>
2025-10-17 16:22:33 +0100

    feat: integrate Polar authentication and subscription management

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,10 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
 
+import { polar, checkout, portal, usage, webhooks } from "@polar-sh/better-auth";
+import { Polar } from "@polar-sh/sdk";
+import { polarClient } from "./polar";
+
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -11,4 +15,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar ({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId:'4be5e7d4-c83e-498d-a2fb-4de3bb6c8638',
+              slug:'pro',
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL!,
+          authenticatedUsersOnly: true,
+        }),
+        portal()
+      ]
+    })
+  ]
 });
