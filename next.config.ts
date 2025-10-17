0f5ff9a6d05397241f9c5e5f78e5dfd90ff8948a
Gerald Baalham <geraldbaalham@live.co.uk>
2025-10-17 14:39:02 +0100

    feat: implement authentication and navigation structure with sidebar and header components

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
 import {withSentryConfig} from "@sentry/nextjs";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/workflows',
+        permanent: false
+      }
+    ]
+  }
 };
 
 export default withSentryConfig(nextConfig, {
+
+  
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
---
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -0,0 +1,9 @@
+import { requireAuth } from "@/lib/auth-utils"
+
+const Page = async () => {
+    await requireAuth()
+  return (
+    <div>Workflows Page</div>
+  )
+}
+export default Page
